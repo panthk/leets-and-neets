d13cc1ec4a349c0e5787e6431a7e4f8608231ccd
panthk <124208649+panthk@users.noreply.github.com>
2026-04-30 23:11:19 -0400

    Time: 11 ms (76.14%) | Memory: 19.4 MB (75.16%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        letters = {}
+
+        for letter in s:
+            if letter in letters:
+                letters[letter] = letters[letter] + 1
+                continue
+            
+            letters[letter] = 1
+        
+        for letter in t:
+            if letter not in letters:
+                return False
+                
+            letters[letter] = letters[letter] - 1
+            if letters[letter] == -1:
+                return False
+        
+        return True
+        
+
+            
